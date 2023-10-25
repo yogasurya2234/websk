6fc03e95d2e1a7fd01b7abf57b6cb4609e2c9ab5
yogasurya2234 <yogasurya069@gmail.com>
2023-10-25 19:49:24 +0800

    belum selesai

--- app/Http/Controllers/MKController.php
+++ app/Http/Controllers/MKController.php
@@ -6,15 +6,40 @@ use Illuminate\Http\Request;
 
 class MKController extends Controller
 {
+    private $data = [
+        [
+            'ID' => "123456",
+            'nama' => "elektro",
+            'jurusan' => "sistem komputer",
+        ],
+        [
+            'ID' => "2201020089",
+            'nama' => "dip",
+            'jurusan' => "sistem komputer",
+        ],
+        [
+            'ID' => "345678",
+            'nama' => "network infastruktur",
+            'jurusan' => "sistem komputer",
+        ],
+        [
+            'ID' => "456789",
+            'nama' => "web progreming",
+            'jurusan' => "sistem komputer",
+        ],
+    ];
     public function index()
     {
+        return view('MK.index', ['data' => $this->data]);
     }
 
     public function create()
     {
+        return view('MK.create');
     }
 
     public function show($id)
     {
+        return view('MK.edit', ['data' => $this->data[$id], 'id' => $id]);
     }
 }
